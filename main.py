b1c74a4957dc9b8104a030d62692e4fd05329c49
Dmitry <astronom.v.cube@gmail.com>
2024-10-29 16:02:17 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from data import *
+from utils import PlotGraph
+
+plotter_circle = PlotGraph()
+plotter_circle.plotting_base_graphs(circle_8_2ghz, circle_10ghz, circle_11_8ghz)
+plt.savefig(f'img/circle.png', dpi = 300)
+plt.close(plotter_circle.fig)
+
+plotter_big_circle = PlotGraph()
+plotter_big_circle.plotting_base_graphs(big_circle_8_2ghz, big_circle_10ghz, big_circle_11_8ghz)
+plt.savefig(f'img/big_circle.png', dpi = 300)
+plt.close(plotter_big_circle.fig)
+
+plotter_max_circle = PlotGraph()
+plotter_max_circle.plotting_base_graphs(max_circle_8_2ghz, max_circle_10ghz, max_circle_11_8ghz)
+plotter_max_circle.plot_additional_graph(cut_x_DOP, max_circle_8_2ghz_dop, label='8.2 GHz additional', linestyle='-', color='k')
+plotter_max_circle.plot_additional_graph(cut_x_DOP, max_circle_10ghz_dop, label='10 GHz additional', linestyle='--', color='k')
+plotter_max_circle.plot_additional_graph(cut_x_DOP, max_circle_11_8ghz_dop, label='11.8 GHz additional', linestyle=':', color='k')
+plt.savefig(f'img/max_circle.png', dpi = 300)
+plt.close(plotter_max_circle.fig)
+
+plotter_square = PlotGraph()
+plotter_square.plotting_base_graphs(square_8_2_ghz, square_10_ghz, square_11_8ghz)
+plt.savefig(f'img/square.png', dpi = 300)
+plt.close(plotter_square.fig)
+
+plotter_nonsymmetrical = PlotGraph()
+plotter_nonsymmetrical.plot_additional_graph(x, nonsymmetrical_8_2_ghz, label='8.2', linestyle='-', color='b')
+plotter_nonsymmetrical.plot_additional_graph(x, nonsymmetrical_10_ghz, label='10 GHz', linestyle='-', color='r')
+plotter_nonsymmetrical.plot_additional_graph(cut_x, nonsymmetrical_11_8ghz, label='11.8 GHz', linestyle='-', color='g')
+plotter_nonsymmetrical.plot_additional_graph(cut_x_DOP, nonsymmetrical_11_8ghz_DOP, label='11.8 GHz additional', linestyle='--', color='k')
+plt.grid(True)
+plt.tight_layout()
+plt.savefig(f'img/nonsymmetrical.png', dpi = 300)
+plt.show()
+plt.close(plotter_nonsymmetrical.fig)
+
---
+++ utils.py
@@ -0,0 +1,58 @@
+from xmlrpc.client import Boolean
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import CubicSpline
+import matplotlib
+matplotlib.rcParams.update({'font.size': 20})
+
+class PlotGraph():
+
+    def __init__(self):
+        self.fig, self.ax = plt.subplots(num="График", figsize=(12, 8))
+        self.x = np.linspace(-100, 100, 41)
+        self.cut_x = np.linspace(-60, 60, 25)
+        self.cut_x_DOP = np.linspace(-25, 25, 26)
+
+    def cubic_spline_interpolation(x, y, x_for_spline):
+        cs = CubicSpline(x, y)
+        new_y = cs(x_for_spline)
+        return new_y
+
+    def dbm2linear(dbm_list):
+        linear_list = [10 ** (dbm_value / 10) for dbm_value in dbm_list]
+        return linear_list
+
+    def plotting_base_graphs(self, array82ghz: np.ndarray, array10ghz: np.ndarray, array118ghz: np.ndarray, x_arr: str = 'normal', dbm2linear: Boolean = False):
+
+        if x_arr == 'normal':
+            x = self.x
+        elif x_arr == 'cut':
+            x = self.cut_x
+
+        if dbm2linear:
+            array82ghz = PlotGraph.dbm2linear(array82ghz)
+            array10ghz = PlotGraph.dbm2linear(array10ghz)
+            array118ghz = PlotGraph.dbm2linear(array118ghz)
+
+        x_for_spline = np.linspace(x[0], x[-1], 4096)
+
+        array82ghz = PlotGraph.cubic_spline_interpolation(x, array82ghz, x_for_spline)
+        array10ghz = PlotGraph.cubic_spline_interpolation(x, array10ghz, x_for_spline)
+        array118ghz = PlotGraph.cubic_spline_interpolation(x, array118ghz, x_for_spline)
+
+        self.ax.plot(x_for_spline, array82ghz, label='8.2 GHz', color='b', linewidth = 5)
+        self.ax.plot(x_for_spline, array10ghz, label='10 GHz', color='r', linewidth = 5)
+        self.ax.plot(x_for_spline, array118ghz, label='11.8 GHz', color='g', linewidth = 5)
+
+        self.ax.grid(True)
+        self.ax.legend()
+        plt.tight_layout()
+
+    def plot_additional_graph(self, x_data, y_data, dbm2linear: Boolean = False, **kwargs):
+
+        if dbm2linear:
+            y_data = PlotGraph.dbm2linear(y_data)
+        x_for_spline = np.linspace(x_data[0], x_data[-1], 4096)
+        y_data = PlotGraph.cubic_spline_interpolation(x_data, y_data, x_for_spline)
+        self.ax.plot(x_for_spline, y_data, linewidth = 3, **kwargs)
+        plt.legend()
